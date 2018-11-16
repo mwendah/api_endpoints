19d0de71e327867a83e9d6c1c2843ba21b79d7eb
kelvin mwenda <mwendak2@gmail.com>
2018-11-16 07:14:32 +0300

    requirements

--- app/tests/v1/test_parcel.py
+++ app/tests/v1/test_parcel.py
@@ -6,47 +6,41 @@ from ..api.v1.models  import ParcelModel
 import json
 
 class TestParcelData(unittest.TestCase):
+	def setup(self):
+		self.app=create_app()
+		self.client=self.app.test_client()
+		self.app_context=self.app.app_context()
+		self.app_context.push()
 
-def setup(self):
-self.app=create_app()
-self.client=self.app.test_client()
-self.app_context=self.app.app_context()
-self.app_context.push()
-new_data={
-      'id'="1"
-	"destination"="Nairobi"
-	"recipient"="mr.kanyani"
-	"sender"="mr.monkey"
-	"weight"="23kg"
-	"status"="intransit"
+self.order={
+    "id" : 1,
+	 "destination" : "likoni",
+	"recipient" : "mr C",
+	"sender" : "mr.activer",
+	"weight" : "23",
+"status" : "activer"
         }
 
 
 class  Test_Random(TestParcelData):
-		
-
-def test_post(self):
-	response=self.client.post(
-'/DataDelivery',data=json.dumps(self.data),content_type='application/json')
-	result=json.loads(response.data.decode())
-	self.assertEqual(result['message'],'Hello mate',message'Error in test')
-	assert response.status_code==200
+	def test_cancel_order(self):
+		response=self.client.post('/parcel/cancel',data=json.dumps(self.data),content_type='application/json')
+	    result=json.loads.response.data.decode()
+	    self.assertEqual(result['message'],'parcel canceled',message'Error in test')
+	    assert response.status_code==200
 
 
+	
 
+def test_get(self):
+	response=self.client.get('/user/parcel',data=json.dumps(self.data),content_type='application/json')      	result=json.loads(response.data.decode())
+    self.assertEqual(result['message'],'parcel found',message'Error in test')
+    assert response.status_code==200
 
-	def test_get(self):
-        response=self.client.get(
-        '/DataDelivery',data=json.dumps(self.data),content_type='application/json')
-        	result=json.loads(response.data.decode())
-        	self.assertEqual(result['message'],'Hello mate',message'Error in test')
-        	assert response.status_code==200
-
-		def test_put(self):
-response=self.client.get(
-'/DataDelivery',data=json.dumps(self.data),content_type='application/json')
-	result=json.loads(response.data.decode())
-	self.assertEqual(result['message'],'Hello mate',message'Error in test')
+def test_put(self):
+	response=self.client.get('/parcel',data=json.dumps(self.data),content_type='application/json')
+    result=json.loads(response.data.decode())
+    self.assertEqual(result['message'],'Hello mate',message'Error in test')
 	assert response.status_code==400
 
 
